9c41a35f7cc7cf0d7061a760da0e81a272c4069d
Pablo Mosteiro <pablo.mosteiro@roma1.infn.it>
2018-08-19 23:01:37 +0100

    compare lsqr and lsmr

--- lsqr.py
+++ lsqr.py
@@ -4,6 +4,7 @@ import numpy as np
 import program as pr
 import matplotlib.pyplot as mp
 import random as rd
+import time as tm
 
 # Create CSR matrix from lists of rows, columns and values
 def convert_to_sparse_matrix(mal, nRows, nCols):
@@ -46,7 +47,8 @@ def lsqr_single(A, b, nCols, aTol, bTol, verbose):
 # Compute LSQR for a given A, b
 # A is a Python sparse matrix
 def lsqr_single_sparse(A, b, aTol, bTol, verbose):
-    return la.lsqr(A, b, atol = aTol, btol = bTol, show = verbose)[:3]
+    r = la.lsqr(A, b, atol = aTol, btol = bTol, show = verbose)[:3]
+    return r
 
 # Compute the cost function; A must be of the form (rows, cols, vals)
 def cost(A, b, x):
@@ -54,12 +56,40 @@ def cost(A, b, x):
 
 # Try LSQR with the entire data
 # aTol and bTol represent the actual error in the input data
-def lsqr(aFile, bFile, nCols, aTol = 1e-6, bTol = 1e-7):
+def lsqr(aFile, bFile, nCols, aTol = 1e-6, bTol = 1e-7, verbose = True):
     # Read the data
     (aLists,b) = pr.read_data(aFile, bFile)
-    (aSparse, (x, reason, nIters)) = lsqr_single(aLists, b, nCols, aTol, bTol, True)
+    (aSparse, (x, reason, nIters)) = lsqr_single(aLists, b, nCols, aTol, bTol, verbose)
     L = cost(aLists, b, x)
     return (x, L, reason, nIters, aLists, b, aSparse)
+
+# Generate random b vectors
+def gen_rand_b(A, realB, lenX, nTests, xMax, sc):
+    realBNorm = np.sqrt(np.sum(realB * realB))
+
+    # Generate nTests random x vectors
+    print("Generating random xs...")
+    xs = np.array([[rd.randint(1, xMax * sc)/float(sc) for j in range(lenX)] for i in range(nTests)])
+    # Generate the corresponding b vectors
+    print("Initialising the corresponding bs...")
+    fakeBs = [np.zeros(len(realB)) for i in range(len(xs))]
+    print("Filling the values of bs...")
+    for aRow in range(len(A[0])):
+        row = A[0][aRow]
+        col = A[1][aRow]
+        val = A[2][aRow]
+        for i in range(len(xs)):
+            x = xs[i]
+            b = fakeBs[i]
+            b[row] += (val * x[col])
+    for i in range(len(fakeBs)):
+        fakeB = fakeBs[i]
+        fakeX = xs[i]
+        fakeBNorm = np.sqrt(np.sum(fakeB * fakeB))
+        factor = realBNorm / fakeBNorm
+        fakeB *= factor
+        fakeX *= factor
+    return (xs, fakeBs)
 
 # Try LSQR with multiple b vectors
 def lsqr_test(aTol, bTol, nTests):
@@ -69,31 +99,13 @@ def lsqr_test(aTol, bTol, nTests):
     maxXValue = 10
     multFac = 1. / bTol
 
-    # Generate nTests random x vectors
-    print("Generating random xs...")
-    xs = [[rd.randint(1, maxXValue * multFac)/float(multFac) for j in range(lenX)] for i in range(nTests)]
-    # Generate the corresponding b vectors
     print("Reading the real data...")
     aAndRealB = pr.read_data('Archive/a.txt', 'Archive/b.txt')
     A = aAndRealB[0]
     realB = np.array(aAndRealB[1])
-    realBNorm = np.sqrt(np.sum(realB * realB))
-    print("The real b has norm " + str(realBNorm))
-    print("Initialising the corresponding bs...")
-    bs = [np.zeros(len(realB)) for i in range(len(xs))]
-    print("Filling the values of bs...")
-    for aRow in range(len(A[0])):
-        row = A[0][aRow]
-        col = A[1][aRow]
-        val = A[2][aRow]
-        for i in range(len(xs)):
-            x = xs[i]
-            b = bs[i]
-            b[row] += (val * x[col])
-    for fakeB in bs:
-        fakeBNorm = np.sqrt(np.sum(fakeB * fakeB))
-        factor = realBNorm / fakeBNorm
-        fakeB *= factor
+
+    print('Getting random b vectors...')
+    (xs, bs) = gen_rand_b(A, realB, lenX, nTests, maxXValue, multFac)
 
     # For each b vector, find x using lsqr
     print("Converting to sparse matrix...")
@@ -110,7 +122,9 @@ def lsqr_test(aTol, bTol, nTests):
         iters.append(nIters)
 
         # Compare the x obtained with the one used in the input using program.py
-        pr.compare_x_arrays(x, xs[i])
+        xFracDiff = pr.compare_x_arrays(x, xs[i])[2]
+        if xFracDiff > maxFracDiff:
+            print('ERROR: wrong x result for iteration ' + str(i))
 
         # Make sure the cost was less than maxL in all cases
         L = cost(A, b, x)
@@ -148,15 +162,15 @@ def gcrot():
     (x, info) = la.gcrotmk(aSparse, b)
     return (x, info)
 
-def lsmr():
+def lsmr(aTol, bTol):
     aFile = 'Archive/a.txt'
     bFile = 'Archive/b.txt'
     nCols = 100000
 
     (A,b) = pr.read_data(aFile, bFile)
     aSparse = convert_to_sparse_matrix(A, len(b), nCols)
-    r = la.lsmr(aSparse, b, show = True)
-    return r
+    r = la.lsmr(aSparse, b, show = True, atol = aTol, btol = bTol)
+    return (r, A, b)
 
 def test_tol():
     aT = 1e-6
@@ -180,3 +194,84 @@ def test_tol():
     print("Norm R: " + str(normR))
     print("Norm B: " + str(normB))
     print("L: " + str(L))
+
+# Compare lsqr and lsmr
+# This method shows a consistently slower performance for LSMR
+def compare_algos():
+    aFile = 'Archive/A.txt'
+    bFile = 'Archive/b.txt'
+    nCols = 100000
+    maxL = 1e-3
+    aTol = 1e-6
+    bTol = 1e-7
+    maxErrorL = 1e-2
+    nFakeTests = 10
+    maxXValue = 10
+    multFac = 1. / aTol
+    
+    # Read the data
+    (aLists,b) = pr.read_data(aFile, bFile)
+    # Convert to a sparse matrix
+    aSparse = convert_to_sparse_matrix(aLists, len(b), nCols)
+
+    # Try LSQR
+    t0 = tm.time()
+    r = la.lsqr(aSparse, b, atol = aTol, btol = bTol, show = False)
+    t1 = tm.time()
+    print('LSQR: ' + str(t1 - t0) + ' s')
+    x = r[0]
+    dv = pr.difference_vector(aLists, x, b)
+    L = pr.cost(dv)
+    res = 0.5 * r[3] * r[3]
+    if np.abs(L - res) / res > maxErrorL or L > maxL:
+        print('ERROR: wrong cost for LSQR')
+    
+    # Try LSMR
+    t0 = tm.time()
+    r = la.lsmr(aSparse, b, atol = aTol, btol = bTol, show = False)
+    t1 = tm.time()
+    print('LSMR: ' + str(t1 - t0) + ' s')
+    x = r[0]
+    dv = pr.difference_vector(aLists, x, b)
+    L = pr.cost(dv)
+    res = 0.5 * r[3] * r[3]
+    if np.abs(L - res) / res > maxErrorL or L > maxL:
+        print('ERROR: wrong cost for LSMR')
+
+    # Now try with fake data
+    (xs, bs) = gen_rand_b(aLists, np.array(b), nCols, nFakeTests, maxXValue, multFac)
+    lsqrTimes = []
+    lsmrTimes = []
+    for i in range(len(bs)):
+        t0 = tm.time()
+        r = la.lsmr(aSparse, bs[i], atol = aTol, btol = bTol)
+        t1 = tm.time()
+        lsmrTimes.append(t1 - t0)
+        x = r[0]
+        dv = pr.difference_vector(aLists, x, bs[i])
+        L = pr.cost(dv)
+        res = 0.5 * r[3] * r[3]
+        if np.abs(L - res) / res > maxErrorL or L > maxL:
+            print('ERROR: wrong cost for LSMR')
+        dx = np.array([(x[j] - xs[i][j]) for j in range(len(x))])
+        if np.sqrt(np.sum(dx * dx)) / np.sqrt(np.sum(xs[i] * xs[i])) > maxErrorL:
+            print('ERROR: wrong x result for LSMR')
+        
+        t0 = tm.time()
+        r = la.lsqr(aSparse, bs[i], atol = aTol, btol = bTol)
+        t1 = tm.time()
+        lsqrTimes.append(t1 - t0)
+        x = r[0]
+        dv = pr.difference_vector(aLists, x, bs[i])
+        L = pr.cost(dv)
+        res = 0.5 * r[3] * r[3]
+        if np.abs(L - res) / res > maxErrorL or L > maxL:
+            print('ERROR: wrong cost for LSQR')
+        dx = np.array([(x[j] - xs[i][j]) for j in range(len(x))])
+        if np.sqrt(np.sum(dx * dx)) / np.sqrt(np.sum(xs[i] * xs[i])) > maxErrorL:
+            print('ERROR: wrong x result for LSQR')
+    
+    # Plot the times
+    mp.plot(range(len(lsqrTimes)), lsqrTimes, color='red')
+    mp.plot(range(len(lsmrTimes)), lsmrTimes, color='blue')
+    mp.show()
